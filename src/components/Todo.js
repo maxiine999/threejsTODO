db2e4492a4eaf27252656613a2d6d0f271c4be77
maxiine999 <139379941+maxiine999@users.noreply.github.com>
2024-08-23 10:45:38 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ import { MdOutlineDeleteOutline } from "react-icons/md";
 const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
   return (
     <div className='flex justify-center mb-2'>
-      <div className='flex items-center bg-white drop-shadow-lg rounded-lg p-2 w-full w-[430px] mx-2 mr-7 h-12 text-gray-800'>
+      <div className='flex items-center bg-white drop-shadow-lg max-sm:mx-[15px] rounded-lg p-2 w-[430px] mx-2 mr-7 h-12 text-gray-800'>
         <p
           onClick={() => toggleComplete(task.id)}
           className={`flex-1 cursor-pointer ${task.completed ? 'line-through text-gray-500' : ''}`}
@@ -24,7 +24,7 @@ const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
         </div>
       </div>
     </div>
-  ); 
+  );
 }
 
 export default Todo;
